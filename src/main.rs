904d075dc818dd916a64818ae969f0b786148afa
yuta4j1 <kascado.ys10@gmail.com>
2020-12-14 00:42:15 +0900

    動いた🎉

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
+use std::error::Error;
+use std::path::Path;
+use futures_util::stream::TryStreamExt;
+use hyper::{Body, Client};
+use hyperlocal::{UnixClientExt, Uri};
+
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    // let base_url = "http://localhost/v1.40";
-    let body = reqwest::get("http:localhost/v1.40/containers/json?all=1").await?.text().await?;
-    println!("body: {}", body);
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let path = Path::new("/var/run/docker.sock");
+    let url = Uri::new(path, "/v1.40/containers/json");
+    let client = Client::unix();
+    let response_body = client.get(url.into()).await?.into_body();
+    let bytes = response_body
+        .try_fold(Vec::default(), |mut buf, bytes| async {
+            buf.extend(bytes);
+            Ok(buf)
+        })
+        .await?;
+
+    println!("{}", String::from_utf8(bytes)?);
 
     Ok(())
 }
